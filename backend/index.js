25e06100aed15e6d78d74e7366795e6b20a41c8e
Marcelo Pena <coder.pena@gmail.com>
2022-08-09 19:43:04 +0000

    End-points comentados

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+// Main End-point
 app.get("/", function (req, res) {
   res.send("Hello, World!");
 });
 
+// oi End-point
 app.get("/oi", function (req, res) {
   res.send("Olá, mundo!");
 });
